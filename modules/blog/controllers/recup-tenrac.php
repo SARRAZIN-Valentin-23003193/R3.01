364329fe90f6ba54970901c72cc09a8b79e3f2bf
SARRAZIN-Valentin-23003193 <valentin.sarrazin@etu.univ-amu.fr>
2024-09-29 15:32:00 +0200

    Ajout des fichiers requis pour Tenracs sans les chemins

--- modules/blog/controllers/recup-tenrac.php
+++ modules/blog/controllers/recup-tenrac.php
@@ -1,4 +1,6 @@
 <?php
+
+function insertLigne(){
     $pwd = "Super_Pershing";
     $user = "sarrazin";
     $host = "mysql-sarrazin.alwaysdata.net";
@@ -13,26 +15,72 @@
     or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
     );
 
-    //Requête SQL
-    $query = 'SELECT nom, num, mail, adresse FROM tenrac';
+    $result = mysqli_query($dbLink, 'SELECT COUNT(*) as count FROM Tenracs');
+    $row = mysqli_fetch_assoc($result);
+    $count = (int)$row['count'];
 
-    //Envoie de la requête
-    if(!($dbResult = mysqli_query($dbLink, $query)))
-    {
-        echo 'Erreur de requête<br>';
-        // Affiche le type d'erreur.
-        echo 'Erreur : ' . mysqli_error($dbLink) . '<br>';
-        // Affiche la requête envoyée.
-        echo 'Requête : ' . $query . '<br>';
-        exit();
-    }
+    $i = 1;
+    while($i < ($count+1)){
+        $qnom = 'SELECT Nom_T FROM Tenracs WHERE Tenracid = ' . $i;
+        $nomResult = mysqli_query($dbLink, $qnom);
+        $nomRow = mysqli_fetch_assoc($nomResult);
+        $nom = $nomRow['nom'];
 
-    //Récupère le résultat de la requête
-    while($dbRow = mysqli_fetch_assoc($dbResult)) {
-        $nom = $dbRow['nom'] . ' ';
-        echo $dbRow['num'] . ' ';
-        echo $dbRow['mail'] . ' ';
-        echo $dbRow['adresse'] . ' ';
-        echo '<br><br>';
+        $qnum = 'SELECT NumTel FROM Tenracs WHERE Tenracid = ' . $i;
+        $numResult = mysqli_query($dbLink, $qnum);
+        $numRow = mysqli_fetch_assoc($numResult);
+        $num = $numRow['num'];
+
+        $qmail = 'SELECT Courriel FROM Tenracs WHERE Tenracid = ' . $i;
+        $mailResult = mysqli_query($dbLink, $qmail);
+        $mailRow = mysqli_fetch_assoc($mailResult);
+        $mail = $mailRow['mail'];
+
+        $qadresse = 'SELECT Adresse_T FROM Tenracs WHERE Tenracid = ' . $i;
+        $adresseResult = mysqli_query($dbLink, $qadresse);
+        $adresseRow = mysqli_fetch_assoc($adresseResult);
+        $adresse = $adresseRow['adresse'];
+
+        $qgrade = 'SELECT Grade FROM Tenracs WHERE Tenracid = ' . $i;
+        $gradeResult = mysqli_query($dbLink, $qgrade);
+        $gradeRow = mysqli_fetch_assoc($gradeResult);
+        $grade = $gradeRow['grade'];
+
+        $qtitre = 'SELECT Titre FROM Tenracs WHERE Tenracid = ' . $i;
+        $titreResult = mysqli_query($dbLink, $qtitre);
+        $titreRow = mysqli_fetch_assoc($titreResult);
+        $titre = $titreRow['titre'];
+
+        $qrang = 'SELECT Rang FROM Tenracs WHERE Tenracid = ' . $i;
+        $rangResult = mysqli_query($dbLink, $qrang);
+        $rangRow = mysqli_fetch_assoc($rangResult);
+        $rang = $rangRow['rang'];
+
+        $qdignite = 'SELECT Dignite FROM Tenracs WHERE Tenracid = ' . $i;
+        $digniteResult = mysqli_query($dbLink, $qdignite);
+        $digniteRow = mysqli_fetch_assoc($digniteResult);
+        $dignite = $digniteRow['dignite'];
+
+        $i++;
+
+        initLigne($nom, $num, $mail, $adresse, $grade, $titre, $rang, $dignite);
     }
+}
+
+function initLigne($nom, $num, $mail, $adresse, $grade, $titre, $rang, $dignite){
+    ?>
+    <table class="tableau">
+        <tr>
+            <th class="nom"><?php echo(string) $nom; ?></th>
+            <th class="num"><?php echo(string) $num; ?></th>
+            <th class="mail"><?php echo(string) $mail; ?></th>
+            <th class="adresse"><?php echo(string) $adresse; ?></th>
+            <th class="grade"><?php echo(string) $grade; ?></th>
+            <th class="titre"><?php echo(string) $titre; ?></th>
+            <th class="rang"><?php echo(string) $rang; ?></th>
+            <th class="dignite"><?php echo(string) $dignite; ?></th>
+        </tr>
+    </table>
+    <?php
+}
 ?>
