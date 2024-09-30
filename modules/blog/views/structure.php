580760b8872d714201b6fb9ecf2c966a6b325608
gallo <loic.gallou@etu.univ-amu.fr>
2024-09-30 20:33:15 +0200

    Ajout du formulaire et du fichier ajout-club.php permetant l'ajout de données dans la bd

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -1,9 +1,17 @@
 <?php
 require '/home/tenrac45/www/modules/blog/controllers/blocController.php';
 require __DIR__ . '/HtmlLayout.php';
+require '../controllers/ajout-club.php';
+
 start_page("L'ordre des tenracs");
 addHeader();
 ?>
+    <form action="../../controllers/ajout-club.php" method="post">
+        <label>nom du Club <input type="text" name="nomclub"></label>
+        <label>Adresse <input type="text" name="adressclub"></label>
+        <button type="submit">envoyer</button>
+    </form>
+
     <main>
         <section id="bloc">
             <?php
