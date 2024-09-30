174995779dc3ca07cd7e852d5d8c061e29b29762
fivema <francois.marty@etu.univ-amu.fr>
2024-09-30 19:05:12 +0200

    structure.php fonctionnel

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -3,9 +3,13 @@ require __DIR__ . '/HtmlLayout.php';
 start_page("L'ordre des tenracs");
 addHeader();
 ?>
-
-
-
+<main>
+    <section id="bloc">
+        <?php
+        drawBloc();
+        ?>
+    </section>
+</main>
 <?php
 addFooter();
 end_page();
