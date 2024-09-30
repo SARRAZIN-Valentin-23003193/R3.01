ae79b31778892822f4793bccd4ef69952ccd453d
fivema <francois.marty@etu.univ-amu.fr>
2024-09-30 19:44:16 +0200

    Affichage clubs parfaitement fonctionnel 2

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -1,14 +1,18 @@
 <?php
+require '/home/tenrac45/www/modules/blog/controllers/blocController.php';
 require __DIR__ . '/HtmlLayout.php';
 start_page("L'ordre des tenracs");
 addHeader();
 ?>
-
-
-
+    <main>
+        <section id="bloc">
+            <?php
+            drawBloc();
+            ?>
+        </section>
+    </main>
 <?php
 addFooter();
 end_page();
 
 ?>
-
