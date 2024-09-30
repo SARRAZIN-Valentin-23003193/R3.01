9598b4c14f2def9bea980c8b2f2f64870704a40c
gallo <loic.gallou@etu.univ-amu.fr>
2024-09-30 13:58:43 +0200

    remise à nieau des chemin essaie 2

--- modules/blog/views/HtmlLayout.php
+++ modules/blog/views/HtmlLayout.php
@@ -7,8 +7,8 @@ require __DIR__.'/../../../config.php';
  * @author Hugo Valente
  */
 function start_page($title) {
-    ?>
-<!DOCTYPE html>
+?>
+    <!DOCTYPE html>
 <html lang="fr">
 <head>
     <title><?php echo $title ?></title>
@@ -27,39 +27,39 @@ function start_page($title) {
  * @author Hugo Valente
  */
 function addHeader() { ?>
-<header>
-    <section>
-        <!-- https://medium.com/@mateus2050/hamburguer-menu-html-and-css-only-c06364fa9bfd -->
-        <div id="menuToggle">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
-            <span></span>
-            <div id="div_menu">
-                <ul id="menu">
+    <header>
+        <section>
+            <!-- https://medium.com/@mateus2050/hamburguer-menu-html-and-css-only-c06364fa9bfd -->
+            <div id="menuToggle">
+                <input type="checkbox" />
+                <span></span>
+                <span></span>
+                <span></span>
+                <div id="div_menu">
+                    <ul id="menu">
 
-                    <li><a href="<?php echo base_url('index.php'); ?>">Accueil</a></li>
-                    <li><a href="<?php echo base_url('modules/blog/views/structure.php'); ?>">Clubs</a></li>
-                    <li><a href="<?php echo base_url('modules/blog/views/calendrier.php'); ?>">Date RDV</a></li>
-                    <li><a href="<?php echo base_url('modules/blog/views/plat.php'); ?>">Plats</a></li>
-                    <li><a href="<?php echo base_url('modules/blog/views/tenrac.php'); ?>">Tenrac</a></li>
-                    <li><a href="<?php echo base_url('modules/blog/views/authenthification.php'); ?>">Se connecter</a></li>
-                    <li><a href="#">A propos</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
+                        <li><a href="<?php echo base_url('index.php'); ?>">Accueil</a></li>
+                        <li><a href="<?php echo base_url('modules/blog/views/structure.php'); ?>">Clubs</a></li>
+                        <li><a href="<?php echo base_url('modules/blog/views/calendrier.php'); ?>">Date RDV</a></li>
+                        <li><a href="<?php echo base_url('modules/blog/views/plat.php'); ?>">Plats</a></li>
+                        <li><a href="<?php echo base_url('modules/blog/views/tenrac.php'); ?>">Tenrac</a></li>
+                        <li><a href="<?php echo base_url('modules/blog/views/authenthification.php'); ?>">Se connecter</a></li>
+                        <li><a href="#">A propos</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        <img src="<?php echo base_url('_assets/images/logo_tenrac_sans_fond.webp');?>" class="logo_tenrac_header" alt="logo tenrac">
-    </section>
-    <section class="MainTitle">
-        <h1>L'Ordre des Tenracs</h1>
-        <h2>Bienvenue sur notre site</h2>
-    </section>
-    <section>
-        <img src="<?php echo base_url('_assets/images/login_icon.webp');?>" alt="Login logo" class="logo_login_header"/>
-    </section>
-</header>
-<?php
+            <img src="<?php echo base_url('_assets/images/logo_tenrac_sans_fond.webp');?>" class="logo_tenrac_header" alt="logo tenrac">
+        </section>
+        <section class="MainTitle">
+            <h1>L'Ordre des Tenracs</h1>
+            <h2>Bienvenue sur notre site</h2>
+        </section>
+        <section>
+            <img src="<?php echo base_url('_assets/images/login_icon.webp');?>" alt="Login logo" class="logo_login_header"/>
+        </section>
+    </header>
+    <?php
 }
 
 /***
@@ -81,10 +81,10 @@ function end_page() { ?>
 function addFooter()
 { ?>
 
-<footer>
-    <p>2024 © Gallou Loic - Garcia Léo - Kanboui Jalil - Marty François - Sarrazin Valentin - Thevenet Antoine - Valente Hugo</p>
-</footer>
-<?php
+    <footer>
+        <p>2024 © Gallou Loic - Garcia Léo - Kanboui Jalil - Marty François - Sarrazin Valentin - Thevenet Antoine - Valente Hugo</p>
+    </footer>
+    <?php
 }
 
 ?>
