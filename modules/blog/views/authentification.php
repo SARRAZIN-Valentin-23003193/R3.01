a05cd1dd6cfe812f83cbad43281a20782648cbc4
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2024-10-01 18:11:38 +0200

    Ajout du formulaire d'authentification

--- modules/blog/views/authentification.php
+++ modules/blog/views/authentification.php
@@ -3,8 +3,25 @@ require __DIR__ . '/HtmlLayout.php';
 start_page("L'ordre des tenracs");
 addHeader();
 ?>
+<?php
+if(isset($_SESSION['suid'])) {
+    header('Location: /home/tenrac45/www/index.php');
+}
+if (isset($_SESSION['error'])) {
+    echo $_SESSION['error']; // Afficher le message d'erreur
+    unset($_SESSION['error']); // Supprimer le message après l'affichage
+}
+?>
+<form action="" method="post" >
+    <label for="identifiant">Identifiant :</label>
+    <input type="text" id="identifiant" name="identifiant" required>
 
-<p>ok</p>
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required>
+    <br><br>
+
+    <input type="submit" name="action" value="mailer">
+</form>
 
 <?php
 addFooter();
