a17988bce0429423d0f4702ca0cd6bd9382657a0
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2024-10-01 19:48:35 +0200

    Modification de clubController.php pour correspondre au model MVC

--- modules/blog/controllers/clubController.php
+++ modules/blog/controllers/clubController.php
@@ -1,21 +1,6 @@
 <?php
 require '/home/tenrac45/www/modules/blog/models/clubModel.php';
-
-function connectDB(){
-    $pwd = "projetwebtenrac";
-    $user = "tenrac45";
-    $host = "mysql-tenrac45.alwaysdata.net";
-    $db = "tenrac45_1";
-
-    // Verifie la connection
-    $dbLink = mysqli_connect($host, $user, $pwd)
-    or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-
-    // Choix de la base
-    mysqli_select_db($dbLink , $db)
-    or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-    return $dbLink;
-}
+require 'connectDataBaseController.php';
 
 function drawClub($currentPage = 1, $postsPerPage = 5){
     $dbLink = connectDB();
