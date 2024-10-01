d20fd1698b5ccb42dc2ce5aec682276cc1580be2
gallo <loic.gallou@etu.univ-amu.fr>
2024-10-01 23:11:17 +0200

    Probleme de chemin reglé

--- modules/blog/views/structure.php
+++ modules/blog/views/structure.php
@@ -8,7 +8,7 @@ require '../controllers/sup-club.php';
 start_page("L'ordre des tenracs");
 addHeader();
 ?>
-    <form action="../controllers/ajout-club.php" method="post">
+    <form action="../../controllers/ajout-club.php" method="post">
         <label>nom du Club <input type="text" name="nomclub"></label>
         <label>Adresse <input type="text" name="adressclub"></label>
         <button type="submit">envoyer</button>
