3ada84251e36b752d9dd2c62ba2827a0c2c17405
t22018451 <antoine.thevenet@etu.univ-amu.fr>
2024-10-02 17:30:06 +0200

    plat modifié

--- modules/blog/models/platsModel.php
+++ modules/blog/models/platsModel.php
@@ -14,6 +14,32 @@ class Plat {
             echo "Erreur de connexion : " . $e->getMessage();
         }
     }
+    public function fetchPlats($currentPage = 1, $postsPerPage = 5) {
+        // Get the total number of posts
+        $result = mysqli_query($this->conn, 'SELECT COUNT(*) as count FROM Plat');
+        $row = mysqli_fetch_assoc($result);
+        $totalPosts = (int)$row['count'];
+
+        // Calculate the offset
+        $offset = ($currentPage - 1) * $postsPerPage;
+
+        // Fetch the posts for the current page
+        $query = 'SELECT Plat.Nom_P, Sauce_Accompagnement.Nom_S, Ingredient.Nom_I, Accompagnement.Nom_Accomp
+                FROM Plat
+                JOIN Accompagne ON Plat.Plat_id = Accompagne.Plat_id
+                JOIN Accompagnement ON Accompagne.Accomp_id = Accompagnement.Accomp_id
+                JOIN Sauce ON Plat.Plat_id = Sauce.Plat_id
+                JOIN Sauce_Accompagnement ON Sauce.Sauce_id = Sauce_Accompagnement.Sauce_id
+                JOIN Ingredient ON Sauce_Accompagnement.Ingredient_id = Ingredient.Ingredient_id LIMIT ' . $postsPerPage . ' OFFSET ' . $offset;
+        $result = mysqli_query($this->conn, $query);
+
+        plats = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $plats[] = $row;
+        }
+
+        return [$plats, $totalPosts];
+    }
 
     // Méthode pour ajouter un plat
     public function ajouterPlat($plat) {
