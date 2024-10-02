8defc5fb5161c7743aa22e19eccccd00ee4b2266
t22018451 <antoine.thevenet@etu.univ-amu.fr>
2024-10-02 14:15:46 +0200

    changement require

--- modules/blog/views/plat.php
+++ modules/blog/views/plat.php
@@ -1,5 +1,5 @@
 <?php
-require '/home/tenrac45/www/modules/blog/controllers/platModel.php';
+require '/home/tenrac45/www/modules/blog/models/platModel.php';
 require __DIR__ . '/HtmlLayout.php';
 start_page("L'ordre des tenracs");
 addHeader();
