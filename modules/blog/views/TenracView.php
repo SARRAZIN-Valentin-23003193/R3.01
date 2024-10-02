c91446fd35c1d4cdc82f0ae13463d774ab86991f
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2024-10-03 00:56:30 +0200

    Gestion de la restriction de l'ajout d'un tenrac si non connecté

--- modules/blog/views/TenracView.php
+++ modules/blog/views/TenracView.php
@@ -33,48 +33,55 @@ class TenracView {
         <button type="submit">Rechercher</button>
     </form>
 
-    <form action="?action=ajoutTenrac" method="post">
-        <label>Nom du tenrac<input type="text" name="nom"></label>
-        <label>Numéro de téléphone<input type="text" name="num"></label>
-        <label>Mail<input type="text" name="mail"></label>
-        <label>Adresse<input type="text" name="adresse"></label>
-        <label>Grade
-            <select name="grade">
-                <option value="Affilié" selected>Affilié</option>
-                <option value="Sympathisant">Sympathisant</option>
-                <option value="Adhérent">Adhérent</option>
-                <option value="Chevalier">Chevalier</option>
-                <option value="Dame">Dame</option>
-                <option value="Grand chevalier">Grand Chevalier</option>
-                <option value="Haute dame">Haute Dame</option>
-                <option value="Commandeur">Commandeur</option>
-                <option value="Grand Croix">Grand'Croix</option>
-            </select>
-        </label>
-        <label>Rang
-            <select name="rang">
-                <option value="Novice" selected>Novice</option>
-                <option value="Compagnon">Compagnon</option>
-            </select>
-        </label>
-        <label>Titre
-            <select name="titre">
-                <option value="Pas de titre" selected>Pas de titre</option>
-                <option value="Philanthrope">Philanthrope</option>
-                <option value="Protecteur">Protecteur</option>
-                <option value="Honorable">Honorable</option>
-            </select>
-        </label>
-        <label>Dignité
-            <select name="dignite">
-                <option value="Pas de dignite" selected>Pas de dignité</option>
-                <option value="Maitre">Maître</option>
-                <option value="Grand Chancelier">Grand Chancelier</option>
-                <option value="Grand Maitre">Grand Maître</option>
-            </select>
-        </label>
-        <button type="submit">Ajouter le Tenrac</button>
-    </form>
+    <?php
+    if(isset($_SESSION['suid'])){
+    ?>
+        <form action="?action=ajoutTenrac" method="post">
+            <label>Nom du tenrac<input type="text" name="nom"></label>
+            <label>Numéro de téléphone<input type="text" name="num"></label>
+            <label>Mail<input type="text" name="mail"></label>
+            <label>Adresse<input type="text" name="adresse"></label>
+            <label>Grade
+                <select name="grade">
+                    <option value="Affilié" selected>Affilié</option>
+                    <option value="Sympathisant">Sympathisant</option>
+                    <option value="Adhérent">Adhérent</option>
+                    <option value="Chevalier">Chevalier</option>
+                    <option value="Dame">Dame</option>
+                    <option value="Grand chevalier">Grand Chevalier</option>
+                    <option value="Haute dame">Haute Dame</option>
+                    <option value="Commandeur">Commandeur</option>
+                    <option value="Grand Croix">Grand'Croix</option>
+                </select>
+            </label>
+            <label>Rang
+                <select name="rang">
+                    <option value="Novice" selected>Novice</option>
+                    <option value="Compagnon">Compagnon</option>
+                </select>
+            </label>
+            <label>Titre
+                <select name="titre">
+                    <option value="Pas de titre" selected>Pas de titre</option>
+                    <option value="Philanthrope">Philanthrope</option>
+                    <option value="Protecteur">Protecteur</option>
+                    <option value="Honorable">Honorable</option>
+                </select>
+            </label>
+            <label>Dignité
+                <select name="dignite">
+                    <option value="Pas de dignite" selected>Pas de dignité</option>
+                    <option value="Maitre">Maître</option>
+                    <option value="Grand Chancelier">Grand Chancelier</option>
+                    <option value="Grand Maitre">Grand Maître</option>
+                </select>
+            </label>
+            <button type="submit">Ajouter le Tenrac</button>
+        </form>
+    <?php
+    }
+    ?>
+
     <?php
         (new HtmlLayout("Tenrac", ob_get_clean()))->show();
     }
