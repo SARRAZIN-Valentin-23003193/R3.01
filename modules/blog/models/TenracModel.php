35a3e4954f30d9902ac1c5b449a7cd7d1d82ee85
gallo <loic.gallou@etu.univ-amu.fr>
2024-10-03 01:25:20 +0200

    Ajout de la fonctionnalté d'ajout de tenrac

--- modules/blog/models/TenracModel.php
+++ modules/blog/models/TenracModel.php
@@ -38,19 +38,10 @@ class TenracModel {
 
     // Méthode pour ajouter un tenracView
     public function ajouterTenrac($nom, $num, $mail, $adresse, $grade, $titre, $rang, $dignite) {
-        // Connexion à la base de données (adapter les valeurs de connexion)
-        $host = 'mysql-tenrac45.alwaysdata.net';
-        $dbname = 'tenrac45_1';
-        $username = 'tenrac45';
-        $password = 'projetwebtenrac';
-
         try {
-            $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
             // Préparation de la requête d'insertion
-            $sql = "INSERT INTO Tenracs (Nom_T, NumTel, Courriel, Adresse_T, Grade, Titre, Rang, Dignite) VALUES (:nom, :num, :mail, :adresse, :grade, :titre, :rang, :dignite)";
-            $stmt = $conn->prepare($sql);
+            $sql = "INSERT INTO `Tenracs`(`Nom_T`, `NumTel`, `Courriel`, `Adresse_T`, `Grade`, `Titre`, `Rang`, `Dignite`) VALUES (:nom, :num, :mail, :adresse, :grade, :titre, :rang, :dignite)";
+            $stmt = $this->conn->prepare($sql);
             $stmt->bindParam(':nom', $nom);
             $stmt->bindParam(':num', $num);
             $stmt->bindParam(':mail', $mail);
@@ -59,14 +50,11 @@ class TenracModel {
             $stmt->bindParam(':titre', $titre);
             $stmt->bindParam(':rang', $rang);
             $stmt->bindParam(':dignite', $dignite);
-
-            // Exécuter la requête
             $stmt->execute();
-            // echo "Tenrac ajouté avec succès !";
+            echo "Tenrac ajouté avec succès !";
         } catch(PDOException $e) {
-            // echo "Erreur";
+            echo "Erreur : " . $e->getMessage();
         }
-        header('location:https://tenrac45.alwaysdata.net/modules/blog/views/TenracView.php');
     }
 
     // Méthode pour modifier un tenracView
